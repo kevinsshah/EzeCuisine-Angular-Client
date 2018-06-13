d63d46263e67a78eee5e143f7e1307383425a41a
parshva45 <parshva45@gmail.com>
2018-06-13 17:52:02 -0400

    changing component name from home to search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 import {FormsModule} from '@angular/forms';
 import {YummlyServiceClient} from './services/yummly.service.client';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    SearchComponent
   ],
   imports: [
     BrowserModule,
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,12 +2,12 @@ import {Component, OnInit} from '@angular/core';
 import {YummlyServiceClient} from '../services/yummly.service.client';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
 })
 
-export class HomeComponent implements OnInit {
+export class SearchComponent implements OnInit {
 
   constructor(private yummlyService: YummlyServiceClient) { }
 
