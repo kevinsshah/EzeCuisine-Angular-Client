ad879888824b2f04b4a6eb2cd024a39fd6343ad8
parshva45 <parshva45@gmail.com>
2018-06-13 17:44:09 -0400

    completing search pagination

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {YummlyServiceClient} from '../services/yummly.service.client';
 
 @Component({
@@ -6,18 +6,26 @@ import {YummlyServiceClient} from '../services/yummly.service.client';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
   constructor(private yummlyService: YummlyServiceClient) { }
 
   searchInput = '';
   results = [];
+  currentPage = 1;
+  firstPage = 1;
+  lastPage = 0;
 
-  search(input) {
+  search(input, pageNumber) {
+    this.currentPage = pageNumber;
     const query = input.replace(' ', '+');
     this.yummlyService
-      .findAllResults(query)
-      .then(results => this.results = results['matches']);
+      .findAllResults(query, pageNumber)
+      .then(results => {
+        this.lastPage = Math.ceil(results['totalMatchCount'] / 10);
+        this.results = results['matches'];
+      });
   }
 
   ngOnInit() {
--- src/app/services/yummly.service.client.ts
+++ src/app/services/yummly.service.client.ts
@@ -1,10 +1,13 @@
 export class YummlyServiceClient {
 
   YUMMLY_API_URL =
-    'http://api.yummly.com/v1/api/recipes?_app_id=708820df&_app_key=5fca28c93b86e595f47404b7877a94d5&q=QUERY&maxResult=100&start=10';
+    'http://api.yummly.com/v1/api/recipes?_app_id=708820df&_app_key=5fca28c93b86e595f47404b7877a94d5&q=QUERY&maxResult=10&start=STARTINDEX';
 
-  findAllResults(query) {
-    return fetch(this.YUMMLY_API_URL.replace('QUERY', query))
+  findAllResults(query, pageNumber) {
+    const startIndex = (pageNumber - 1) * 10;
+    return fetch(this.YUMMLY_API_URL
+      .replace('QUERY', query)
+      .replace('STARTINDEX', (startIndex).toString()))
       .then(response => response.json());
   }
 }
