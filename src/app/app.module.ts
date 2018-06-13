27ae96c87a7662a47b1dee3daebe40e6a7f90e82
parshva45 <parshva45@gmail.com>
2018-06-13 17:54:22 -0400

    adding home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import {FormsModule} from '@angular/forms';
 import {YummlyServiceClient} from './services/yummly.service.client';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
