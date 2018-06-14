17471f89694d2a6ed35f5b22c3bb4512d1dfbce6
shahkevin95 <shah.kevi@husky.neu.edu>
2018-06-13 23:05:06 -0400

    added images and thumb nails

--- src/app/services/yummly.service.client.ts
+++ src/app/services/yummly.service.client.ts
@@ -1,7 +1,11 @@
 export class YummlyServiceClient {
 
   YUMMLY_SEARCH_API_URL =
-    'http://api.yummly.com/v1/api/recipes?_app_id=708820df&_app_key=5fca28c93b86e595f47404b7877a94d5&q=QUERY&maxResult=10&start=STARTINDEX';
+    'http://api.yummly.com/v1/api/recipes?_app_id=708820df&_app_key=5fca28c93b86e595f47404b7877a94d5&' +
+    'q=QUERY&' +
+    'maxResult=10&' +
+    'start=STARTINDEX&' +
+    'requirePictures=true';
 
   YUMMLY_GET_API_URL =
     'http://api.yummly.com/v1/api/recipe/RECIPEID?_app_id=708820df&_app_key=5fca28c93b86e595f47404b7877a94d5';
@@ -15,8 +19,6 @@ export class YummlyServiceClient {
   }
 
   findMealById(mealId) {
-    console.log(this.YUMMLY_GET_API_URL
-      .replace('RECIPEID', mealId));
     return fetch(this.YUMMLY_GET_API_URL
       .replace('RECIPEID', mealId))
       .then(response => response.json());
