1dbb9c648c0fa229098be0ff2c42ce39e2399da8
parshva45 <parshva45@gmail.com>
2018-06-15 01:45:49 -0400

    adding cards at home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {YummlyServiceClient} from '../services/yummly.service.client';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private yummlyService: YummlyServiceClient,
+              private router: Router) {
+    this.getStartRecipes();
+  }
 
+  dishes = [];
   slides = [
     {
       imageSrc: 'http://www.hhdenver.com/data1/images/fullhdwallpaperfood65.jpg',
@@ -26,6 +32,17 @@ export class HomeComponent implements OnInit {
       subHeading: 'Sub heading'
     }
   ];
+
+  getStartRecipes() {
+    this.yummlyService.getStartRecipes()
+      .then(response => {
+        this.dishes = response['matches'];
+      });
+  }
+
+  navigate(yummlyId, recipeName) {
+    this.router.navigate(['search/' + recipeName + '/' + yummlyId]);
+  }
 
   ngOnInit() {
   }
--- src/app/services/yummly.service.client.ts
+++ src/app/services/yummly.service.client.ts
@@ -10,6 +10,19 @@ export class YummlyServiceClient {
   YUMMLY_GET_API_URL =
     'http://api.yummly.com/v1/api/recipe/RECIPEID?_app_id=708820df&_app_key=5fca28c93b86e595f47404b7877a94d5';
 
+  YUMMLY_START_API_URL =
+    'http://api.yummly.com/v1/api/recipes?_app_id=708820df&_app_key=5fca28c93b86e595f47404b7877a94d5&' +
+    '&q=&' +
+    'maxResult=15&' +
+    'start=0&' +
+    'requirePictures=true&' +
+    'allowedCuisine[]=cuisine^cuisine-american';
+
+  getStartRecipes() {
+    return fetch(this.YUMMLY_START_API_URL)
+      .then(response => response.json());
+  }
+
   findAllRecipes(query, pageNumber) {
     const startIndex = (pageNumber - 1) * 10;
     return fetch(this.YUMMLY_SEARCH_API_URL
