89e43e59ae8889a80204b0fa1a5798b528b8ac9a
parshva45 <parshva45@gmail.com>
2018-06-19 19:55:58 -0400

    fix

--- src/app/services/yummly.service.client.ts
+++ src/app/services/yummly.service.client.ts
@@ -1,17 +1,17 @@
 export class YummlyServiceClient {
 
   YUMMLY_SEARCH_API_URL =
-    'http://api.yummly.com/v1/api/recipes?_app_id=708820df&_app_key=5fca28c93b86e595f47404b7877a94d5&' +
+    'https://api.yummly.com/v1/api/recipes?_app_id=708820df&_app_key=5fca28c93b86e595f47404b7877a94d5&' +
     'q=QUERY&' +
     'maxResult=10&' +
     'start=STARTINDEX&' +
     'requirePictures=true';
 
   YUMMLY_GET_API_URL =
-    'http://api.yummly.com/v1/api/recipe/RECIPEID?_app_id=708820df&_app_key=5fca28c93b86e595f47404b7877a94d5';
+    'https://api.yummly.com/v1/api/recipe/RECIPEID?_app_id=708820df&_app_key=5fca28c93b86e595f47404b7877a94d5';
 
   YUMMLY_START_API_URL =
-    'http://api.yummly.com/v1/api/recipes?_app_id=708820df&_app_key=5fca28c93b86e595f47404b7877a94d5&' +
+    'https://api.yummly.com/v1/api/recipes?_app_id=708820df&_app_key=5fca28c93b86e595f47404b7877a94d5&' +
     '&q=&' +
     'maxResult=15&' +
     'start=0&' +
