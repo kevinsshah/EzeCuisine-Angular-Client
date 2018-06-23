b6d626eb3b138cbbd5e6764b5b16749df7b5f5ff
parshva45 <parshva45@gmail.com>
2018-06-23 03:00:55 -0400

    rendering users and recipes on admin page

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from '../models/user.model.client';
+import {UserServiceClient} from '../services/user.service.client';
+import {Router} from '@angular/router';
+import {RecipeServiceClient} from '../services/recipe.service.client';
+import {Recipe} from '../models/recipe.model.client';
 
 @Component({
   selector: 'app-admin-page',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserServiceClient,
+              private recipeService: RecipeServiceClient,
+              private router: Router) {
+  }
+
+  chefs: User[] = [];
+  critics: User[] = [];
+  customers: User[] = [];
+  recipes: Recipe[] = [];
+  selection = 'Manage Users';
+
+  loadAllUsers() {
+    this.userService
+      .findAllUsers()
+      .then(users => {
+        const allUsers = users
+          .filter(user => user.role !== 'Admin');
+        this.chefs = allUsers
+          .filter(user => user.role === 'Chef');
+        this.critics = allUsers
+          .filter(user => user.role === 'Critic');
+        this.customers = allUsers
+          .filter(user => user.role === 'Customer');
+      });
+  }
+
+  loadAllRecipes() {
+    this.recipeService
+      .findAllRecipes()
+      .then(recipes => {
+        this.recipes = recipes});
+  }
+
+  navigateToProfile(username) {
+    this.router.navigate(['profile/' + username]);
+  }
+
+  changeSelection(selection) {
+    this.selection = selection;
+  }
 
   ngOnInit() {
+    this.loadAllUsers();
+    this.loadAllRecipes();
   }
 
 }
